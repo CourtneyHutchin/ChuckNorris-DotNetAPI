e95e6d442ecb2fd09044e4957194a473651746b7
courtneyhutchin47@gmail.com <courtneyhutchin47@gmail.com>
2020-04-24 09:43:49 -0700

    Added button click and a using to the form

--- ChuckJokeForm/Form1.cs
+++ ChuckJokeForm/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ChuckNorrisAPI;
 
 namespace ChuckJokeForm
 {
@@ -15,6 +16,11 @@ namespace ChuckJokeForm
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void GetJoke_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
